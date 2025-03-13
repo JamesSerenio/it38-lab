09c263222f0c0e75cc98a2330e352275f2d0b65c
JamesSerenio <sereniojames363@gmail.com>
2025-03-13 15:44:24 +0800

    added sql dump

--- db/it38c-2.sql
+++ db/it38c-2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 13, 2025 at 09:37 AM
+-- Generation Time: Mar 13, 2025 at 08:43 AM
 -- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Database: `it38c-2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attendance`
+--
+
+CREATE TABLE `attendance` (
+  `attendance_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `attendance_date` date NOT NULL,
+  `attendance_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `attendance`
+--
+
+INSERT INTO `attendance` (`attendance_id`, `user_id`, `username`, `attendance_date`, `attendance_time`) VALUES
+(1, 2, 'james', '2025-03-13', '2025-03-13 08:21:48'),
+(2, 2, 'james', '2025-03-13', '2025-03-13 08:21:55'),
+(3, 2, 'james', '2025-03-13', '2025-03-13 08:22:32'),
+(4, 2, 'james', '2025-03-13', '2025-03-13 08:23:19'),
+(5, 2, 'james', '2025-03-13', '2025-03-13 08:23:59'),
+(6, 2, 'james', '2025-03-13', '2025-03-13 08:25:27'),
+(7, 2, 'james', '2025-03-13', '2025-03-13 08:28:49'),
+(8, 2, 'james', '2025-03-13', '2025-03-13 08:28:51'),
+(9, 2, 'james', '2025-03-13', '2025-03-13 08:29:46'),
+(10, 2, 'james', '2025-03-13', '2025-03-13 08:30:49'),
+(11, 2, 'james', '2025-03-13', '2025-03-13 08:32:34'),
+(12, 2, 'james', '2025-03-13', '2025-03-13 08:32:43');
 
 -- --------------------------------------------------------
 
@@ -40,7 +72,11 @@ CREATE TABLE `login_logs` (
 INSERT INTO `login_logs` (`login_id`, `user_id`, `login_time`) VALUES
 (1, 1, '2025-02-13 16:33:16'),
 (2, 2, '2025-02-13 16:33:30'),
-(3, 1, '2025-02-13 16:34:03');
+(3, 1, '2025-02-13 16:34:03'),
+(4, 2, '2025-03-13 15:21:20'),
+(5, 2, '2025-03-13 15:23:59'),
+(6, 2, '2025-03-13 15:30:47'),
+(7, 2, '2025-03-13 15:32:32');
 
 -- --------------------------------------------------------
 
@@ -50,7 +86,7 @@ INSERT INTO `login_logs` (`login_id`, `user_id`, `login_time`) VALUES
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL UNIQUE,  -- Set username as UNIQUE
+  `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `user_type` enum('admin','user') NOT NULL DEFAULT 'user',
   `last_login` datetime DEFAULT NULL,
@@ -63,11 +99,19 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `user_type`, `last_login`, `created_at`) VALUES
 (1, 'admin', '$2y$10$Yuas.HIUbbqiIDxCA4OuDuDP/RBey.OWTc2erzEigzu58T4nv/plW', 'admin', '2025-02-13 16:34:03', '2025-02-13 08:32:47'),
-(2, 'james', '$2y$10$q6CfZDAwXzNp2gIP7nqlq.OJCWGqakOakT0kwgSNCSz2E0TFnZyaG', 'user', '2025-02-13 16:33:30', '2025-02-13 08:33:09');
+(2, 'james', '$2y$10$q6CfZDAwXzNp2gIP7nqlq.OJCWGqakOakT0kwgSNCSz2E0TFnZyaG', 'user', '2025-03-13 15:32:32', '2025-02-13 08:33:09');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `attendance`
+--
+ALTER TABLE `attendance`
+  ADD PRIMARY KEY (`attendance_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `username` (`username`);
 
 --
 -- Indexes for table `login_logs`
@@ -79,17 +123,24 @@ ALTER TABLE `login_logs`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `attendance`
+--
+ALTER TABLE `attendance`
+  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
 -- AUTO_INCREMENT for table `login_logs`
 --
 ALTER TABLE `login_logs`
-  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -97,29 +148,16 @@ ALTER TABLE `login_logs`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `attendance`
+-- Constraints for dumped tables
 --
 
-CREATE TABLE `attendance` (
-  `attendance_id` int(11) NOT NULL ,
-  `user_id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `attendance_date` date NOT NULL,
-  `attendance_time` datetime NOT NULL,
-  PRIMARY KEY (`attendance_id`),
-  FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE CASCADE,
-  FOREIGN KEY (`username`) REFERENCES `users`(`username`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- Set auto-increment for attendance_id
+--
+-- Constraints for table `attendance`
+--
 ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO-- Set auto-increment for attendance_id
-ALTER TABLE `attendance`
-  MODIFY `attendance_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
+  ADD CONSTRAINT `attendance_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `attendance_ibfk_2` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
